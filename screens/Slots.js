7c7bd743e5dde1b53d84d49c378d32a47ddddb5c
Shruti3004 <mail2shruti.ag@gmail.com>
2021-06-19 04:02:58 +0530

    added slots page

--- screens/Slots.js
+++ screens/Slots.js
@@ -1,10 +1,39 @@
-import React from "react";
-import { StyleSheet, Text } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import CalendarPicker from "react-native-calendar-picker";
 
-function Slots({ navigation }) {
-  return <Text>SLOTS</Text>;
+export default function Slots({ navigation, route }) {
+  const [selectedDate, setSelectedDate] = useState("");
+  const startDate = selectedDate ? selectedDate.toString() : "";
+  return (
+    <View style={styles.container}>
+      <CalendarPicker
+        onDateChange={(date) => {
+          setSelectedDate(date);
+          console.log(route.params);
+          navigation.navigate("SlotDetails");
+        }}
+        selectedDayColor="#420a83"
+        selectedDayTextColor="#FFFFFF"
+        textStyle={{ fontFamily: "font-demi" }}
+        initialDate={Date.now()}
+      />
+      <View>
+        <Text style={styles.text}>SELECTED DATE:{startDate}</Text>
+      </View>
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({});
 
-export default Slots;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FFFFFF",
+    marginTop: 20,
+    fontFamily: "font-medium",
+  },
+  text: {
+    fontFamily: "font-medium",
+  },
+});
